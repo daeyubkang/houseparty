9f25388197f13e84714011866c4128ff0b54902b
daeyubkang <yub102447@gmail.com>
2023-09-02 12:19:51 +0900

    333

--- controller/Csignin.js
+++ controller/Csignin.js
@@ -3,6 +3,17 @@ const bcrypt = require("bcrypt");
 const saltNumber = 10;
 const SECRET = "secretKey";
 const jwt = require("jsonwebtoken");
+const express = require("express");
+const app = express();
+const session = require("express-session");
+
+app.use(
+  session({
+    secret: "mySessionKey",
+    resave: false, //덮어쓰기 가능 여부 결정
+    saveUninitialized: true,
+  })
+);
 
 const comparePassword = (password, dbPassword) => {
   return bcrypt.compareSync(password, dbPassword);
@@ -19,6 +30,8 @@ exports.signinPost = (req, res) => {
     console.log("123", result);
     if (result != null) {
       const compare = comparePassword(req.body.pw, result.dataValues.pw);
+      req.session.name = req.body.userid;
+      req.session.password = req.body.pw;
       res.send({ result: compare });
     } else {
       res.send({ result: false });
