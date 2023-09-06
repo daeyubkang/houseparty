6c6f065c34e69107ffa3d8cd97156c72ee6fe6e4
daeyubkang <yub102447@gmail.com>
2023-09-06 13:55:51 +0900

    230906

--- controller/Csocket.js
+++ controller/Csocket.js
@@ -1,10 +1,30 @@
 const { Users, Parties, Chat, ChatMessage } = require("../models");
-const roomList = [];
 
 exports.connection = (io, socket) => {
   console.log("접속");
+
   //채팅방 목록 보내기
-  socket.emit("roomList", roomList);
+  socket.on("roomList", async (userName, cb) => {
+    const roomList = [];
+    let chatList = await Chat.findAll({
+      attributes: ["roomID", "party_num", "participant_id", "host_id"],
+    });
+    for (let i = 0; i < chatList.length; i++) {
+      console.log("chatLists", chatList[i]);
+      const participant = chatList[i].dataValues.participant_id.split(" ");
+      for (let j = 0; j < participant.length; j++) {
+        if (participant[j] == userName) {
+          roomList.push({
+            roomName: chatList[i].dataValues.roomID,
+            participant_num: participant.length,
+            participant: participant,
+            partiesId: chatList[i].dataValues.participant_id,
+          });
+        }
+      }
+    }
+    cb(roomList);
+  });
 
   //채팅방 만들기 생성
   socket.on("create", async (roomName, userName, partiesId, cb) => {
@@ -38,10 +58,16 @@ exports.connection = (io, socket) => {
       const beforeChat = await ChatMessage.findAll({
         where: { chat_key: roomName },
       });
-      console.log("befoeChat", beforeChat[0].dataValues);
+      console.log("beforeChat", beforeChat);
+      let send_before_chat = [];
       for (let i = 0; i < beforeChat.length; i++) {
         console.log("beforeChat", beforeChat[i].dataValues.send_message);
+        send_before_chat.push({
+          send_id: beforeChat[i].dataValues.send_id,
+          send_message: beforeChat[i].dataValues.send_message,
+        });
       }
+      cb(send_before_chat);
     }
 
     //join(방이름) 해당 방이름으로 없다면 생성. 존재하면 입장
@@ -54,11 +80,12 @@ exports.connection = (io, socket) => {
     socket.to(roomName).emit("notice", `${socket.user}님이 입장하셨습니다`);
 
     //채팅방 목록 갱신
-    if (!roomList.includes(roomName)) {
-      roomList.push(roomName);
-      //갱신된 목록은 전체가 봐야함
-      io.emit("roomList", roomList);
-    }
+    // if (!roomList.includes(roomName)) {
+    //   roomList.push(roomName);
+    //   //갱신된 목록은 전체가 봐야함
+    //   io.emit("roomList", roomList);
+    // }
+
     const usersInRoom = getUsersInRoom(roomName);
     io.to(roomName).emit("userList", usersInRoom);
     cb();
